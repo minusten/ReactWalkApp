9f789748db53664a57c7f3184f40e252a414a895
lucianasiegel <lovebigdaddy303@gmail.com>
2019-07-19 15:06:35 +0300

    added register component with router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Registration from './components/Registration/Registration';
+import Login from './components/LoginComponent/Login';
+import {Switch, Route} from 'react-router-dom'
+
+
 
 function App() {
   return (
+    
     <div className="App">
+      <Switch>
+        <Route exact path='/' component={Login}/>
+        <Route path='/registration' component={Registration}/>
+       
+      </Switch>
+     
+    
+
      
     </div>
   );
