d733c5c5a927bb10e0e05609251c7f6776898c50
lucianasiegel <lovebigdaddy303@gmail.com>
2019-07-30 15:43:38 +0300

    edited redux containers

--- src/components/LoginComponent/index.js
+++ src/components/LoginComponent/index.js
@@ -12,96 +12,96 @@ import axios from 'axios'
 const cookies = new Cookies()
 
 class Login extends Component {
-	state = {
-		db: JSON.parse(JSON.stringify(users)),
-		formData: {
-			email: '',
-			password: ''
-		},
-		status: true,
-		cookiesSaved: false,
-		logedIn: false,
-		fields: {},
-		nameError: false,
-		passwordError: false,
-		users: []
-	}
-	changeFields = (e) => {
-		const { formData } = this.state;
-		formData[e.target.name] = e.target.value;
-		this.setState({ formData })
-		console.log(this.state)
-	}
-	findData = () => {
-		this.state.db.forEach((data, index) => {
-			if (data.email === this.state.email && data.password === this.state.password) {
-				this.setState({ logedIn: true })
-				console.log('Successful login')
-			}
-		})
-		this.setState({ cookiesSaved: cookies.set('loginStatus', !this.state.status) })
-		if (this.state.logedIn) {
-			this.setState({ logenIn: !this.state.logedIn })
-			return this.props.history.push('/login')
-		}
-	}
-	login = () => {
-		console.log('sdfsd')
-		console.log(this.state)
-		const url = `http://10.0.4.20:3006/login`
-		axios.post(url, { user: { email: this.state.formData.email, password: this.state.formData.password } })
-			.then((response) => {
-				console.log(response)
-				cookies.set('token', response.data.user.token)
-				this.props.history.push('/')
-			})
+  state = {
+    db: JSON.parse(JSON.stringify(users)),
+    formData: {
+      email: '',
+      password: ''
+    },
+    status: true,
+    cookiesSaved: false,
+    logedIn: false,
+    fields: {},
+    nameError: false,
+    passwordError: false,
+    users: []
+  }
+  changeFields = (e) => {
+    const { formData } = this.state;
+    formData[e.target.name] = e.target.value;
+    this.setState({ formData })
+    console.log(this.state)
+  }
+  findData = () => {
+    this.state.db.forEach((data, index) => {
+      if (data.email === this.state.email && data.password === this.state.password) {
+        this.setState({ logedIn: true })
+        console.log('Successful login')
+      }
+    })
+    this.setState({ cookiesSaved: cookies.set('loginStatus', !this.state.status) })
+    if (this.state.logedIn) {
+      this.setState({ logenIn: !this.state.logedIn })
+      return this.props.history.push('/login')
+    }
+  }
+  login = () => {
+    console.log('sdfsd')
+    console.log(this.state)
+    const url = `http://10.0.4.20:3006/login`
+    axios.post(url, { user: { email: this.state.formData.email, password: this.state.formData.password } })
+      .then((response) => {
+        console.log(response)
+        cookies.set('token', response.data.user.token)
+        this.props.history.push('/')
+      })
 
-	}
-	render() {
-		if (this.state.logedIn) {
-			return <Redirect to='/' />
-		}
-		return (
-			<div className='loginWrap'>
-				<div className='login'>
-					<p> LOGIN </p>
-					<ValidatorForm>
-						<div className='name'>
-							<img src={avatar} alt="Logo" className='avatar' />
-							<TextValidator
-								label="Email"
-								onChange={this.changeFields}
-								name="email"
-								value={this.state.email}
-								validators={['required', 'isEmail']}
-								errorMessages={['this field is required', 'email is not valid']}
-							/>
-							<br />
-						</div>
-						<div className='password'>
-							<img src={lock} alt="Lock" className='lock' />
-							<br />
-							<TextValidator
-								label="Password"
-								onChange={this.changeFields}
-								name="password"
-								value={this.state.password}
-								validators={['required']}
-								errorMessages={['this field is required']}
-							/>
-							<br />
-						</div>
-					</ValidatorForm>
-					<div className='button'>
-						<Button className='but' variant="outlined" size="small" onClick={this.login}> Sign In </Button>
-						<Button variant="outlined" size="small" > <NavLink to='/registration' className='link'>Sign Up</NavLink> </Button>
-					</div>
+  }
+  render() {
+    if (this.state.logedIn) {
+      return <Redirect to='/' />
+    }
+    return (
+      <div className='loginWrap'>
+        <div className='login'>
+          <p> LOGIN </p>
+          <ValidatorForm>
+            <div className='name'>
+              <img src={avatar} alt="Logo" className='avatar' />
+              <TextValidator
+                label="Email"
+                onChange={this.changeFields}
+                name="email"
+                value={this.state.email}
+                validators={['required', 'isEmail']}
+                errorMessages={['this field is required', 'email is not valid']}
+              />
+              <br />
+            </div>
+            <div className='password'>
+              <img src={lock} alt="Lock" className='lock' />
+              <br />
+              <TextValidator
+                label="Password"
+                onChange={this.changeFields}
+                name="password"
+                value={this.state.password}
+                validators={['required']}
+                errorMessages={['this field is required']}
+              />
+              <br />
+            </div>
+          </ValidatorForm>
+          <div className='button'>
+            <Button className='but' variant="outlined" size="small" onClick={this.login}> Sign In </Button>
+            <Button variant="outlined" size="small" > <NavLink to='/registration' className='link'>Sign Up</NavLink> </Button>
+          </div>
 
-				</div>
-			</div>
+        </div>
+      </div>
 
-		)
-	}
+    )
+  }
 }
 
 
--- src/components/RegisterComponent/index.js
+++ src/components/RegisterComponent/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { NavLink } from 'react-router-dom'
@@ -9,97 +9,98 @@ import Cookies from 'universal-cookie';
 const cookies = new Cookies()
 
 class Register extends Component {
-    state = {
-        formData: {
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: ''
-        },
-        submitted: false,
-        cookiesSaved: false,
-        status: true
-    }
-    validFieldChange = (e) => {
-        const { formData } = this.state;
-        formData[e.target.name] = e.target.value;
-        this.props.myCustomPropsFunc(e.target.name, e.target.value, 'main')
-    }
+  state = {
+    formData: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    },
+    submitted: false,
+    cookiesSaved: false,
+    status: true
+  }
+  validFieldChange = (e) => {
+    const { formData } = this.state;
+    formData[e.target.name] = e.target.value;
+    this.setState({ formData })
+    console.log(this.state)
+  }
 
-    handlePost = () => {
-        axios.post(`http://10.0.4.20:3006/users`, { user: this.state.formData })
-            .then(res => {
-                console.log(res);
-                this.props.history.push('/login')
-            })
-        this.setState({ cookiesSaved: cookies.set('registerStatus', this.state.status) })
-        console.log('Saved', !this.state.cookiesSaved)
-    }
-    render() {
-        const { submitted } = this.state;
-        console.log(this.props)
-        return (
-            <div className='mainWrap'>
-                <div className='formWrap'>
-                    <div className='registerForm'>
-                        <ValidatorForm>
-                            <h2>Registration</h2>
-                            <TextValidator
-                                label="First Name"
-                                onChange={this.validFieldChange}
-                                name='firstName'
-                                value={this.props.firstName}
-                                validators={['required']}
-                                errorMessages={['this field is required']}
+  handlePost = () => {
+    axios.post(`http://10.0.4.20:3006/users`, { user: this.state.formData })
+      .then(res => {
+        console.log(res);
+        this.props.history.push('/login')
+      })
+    this.setState({ cookiesSaved: cookies.set('registerStatus', this.state.status) })
+    console.log('Saved', !this.state.cookiesSaved)
+  }
+  render() {
+    const { submitted } = this.state;
+    console.log(this.props)
+    return (
+      <div className='mainWrap'>
+        <div className='formWrap'>
+          <div className='registerForm'>
+            <ValidatorForm>
+              <h2>Registration</h2>
+              <TextValidator
+                label="First Name"
+                onChange={this.validFieldChange}
+                name='firstName'
+                value={this.state.firstName}
+                validators={['required']}
+                errorMessages={['this field is required']}
 
-                            />
-                            <br />
-                            <TextValidator
-                                label="Last Name"
-                                onChange={this.validFieldChange}
-                                name="lastName"
-                                value={this.props.lastName}
-                                validators={['required']}
-                                errorMessages={['this field is required']}
-                            />
-                            <br />
-                            <TextValidator
-                                label="Email"
-                                onChange={this.validFieldChange}
-                                name="email"
-                                value={this.props.email}
-                                validators={['required', 'isEmail']}
-                                errorMessages={['this field is required', 'email is not valid']}
-                            />
-                            <br />
-                            <TextValidator
-                                label="Password"
-                                onChange={this.validFieldChange}
-                                name="password"
-                                value={this.props.password}
-                                validators={['required']}
-                                errorMessages={['this field is required']}
-                            />
-                            <br />
-                            <Button
-                                className='buttonSubmit'
-                                onClick={this.handlePost}
-                                color="primary"
-                                variant="contained"
-                                type="submit"
-                            >
-                                {
-                                    (submitted && 'Your form is submitted!')
-                                    || (!submitted && 'Submit')
-                                }
-                            </Button>
-                        </ValidatorForm>
-                    </div>
-                    <NavLink to='/login'>   <p> У вас уже есть аккаунт? </p>  </NavLink>
-                </div>
-            </div>
-        );
-    }
+              />
+              <br />
+              <TextValidator
+                label="Last Name"
+                onChange={this.validFieldChange}
+                name="lastName"
+                value={this.state.lastName}
+                validators={['required']}
+                errorMessages={['this field is required']}
+              />
+              <br />
+              <TextValidator
+                label="Email"
+                onChange={this.validFieldChange}
+                name="email"
+                value={this.state.email}
+                validators={['required', 'isEmail']}
+                errorMessages={['this field is required', 'email is not valid']}
+              />
+              <br />
+              <TextValidator
+                label="Password"
+                onChange={this.validFieldChange}
+                name="password"
+                value={this.state.password}
+                validators={['required']}
+                errorMessages={['this field is required']}
+              />
+              <br />
+              <Button
+                className='buttonSubmit'
+                onClick={this.handlePost}
+                color="primary"
+                variant="contained"
+                type="submit"
+              >
+                {
+                  (submitted && 'Your form is submitted!')
+                  || (!submitted && 'Submit')
+                }
+              </Button>
+            </ValidatorForm>
+          </div>
+          <NavLink to='/login'>   <p> У вас уже есть аккаунт? </p>  </NavLink>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Register
